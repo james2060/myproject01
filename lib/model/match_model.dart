9c4a2b5d236e73866ec51e427821cb6a0233b4f0
idelsoo@gmail.com <{idelsoo@gmail.com}>
2023-12-18 06:35:35 +0900

    modified models

--- lib/model/match_model.dart
+++ lib/model/match_model.dart
@@ -1,12 +1,4 @@
-// To parse this JSON data, do
-//
-//     final matchSchedule = matchScheduleFromJson(jsonString);
-
 import 'dart:convert';
-
-MatchSchedule matchScheduleFromJson(String str) => MatchSchedule.fromJson(json.decode(str));
-
-String matchScheduleToJson(MatchSchedule data) => json.encode(data.toJson());
 
 class MatchSchedule {
   Matchinfo matchinfo;
@@ -15,8 +7,19 @@ class MatchSchedule {
     required this.matchinfo,
   });
 
+  MatchSchedule copyWith({
+    Matchinfo? matchinfo,
+  }) =>
+      MatchSchedule(
+        matchinfo: matchinfo ?? this.matchinfo,
+      );
+
+  factory MatchSchedule.fromRawJson(String str) => MatchSchedule.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
   factory MatchSchedule.fromJson(Map<String, dynamic> json) => MatchSchedule(
-    matchinfo: Matchinfo.fromJson(json),
+    matchinfo: Matchinfo.fromJson(json["matchinfo"]),
   );
 
   Map<String, dynamic> toJson() => {
@@ -36,6 +39,8 @@ class Matchinfo {
   int team1Score;
   int team2Score;
   int result;
+  int attendancdCount;
+  int nonappearanceCount;
 
   Matchinfo({
     required this.idx,
@@ -49,9 +54,46 @@ class Matchinfo {
     required this.team1Score,
     required this.team2Score,
     required this.result,
+    required this.attendancdCount,
+    required this.nonappearanceCount,
   });
 
-  factory Matchinfo.fromJson(Map<String, dynamic> json) => Matchinfo(
+  Matchinfo copyWith({
+    int? idx,
+    int? team1Id,
+    int? team2Id,
+    DateTime? starttime,
+    DateTime? endtime,
+    String? location,
+    String? team1Name,
+    String? team2Name,
+    int? team1Score,
+    int? team2Score,
+    int? result,
+    int? attendancdCount,
+    int? nonappearanceCount,
+  }) =>
+      Matchinfo(
+        idx: idx ?? this.idx,
+        team1Id: team1Id ?? this.team1Id,
+        team2Id: team2Id ?? this.team2Id,
+        starttime: starttime ?? this.starttime,
+        endtime: endtime ?? this.endtime,
+        location: location ?? this.location,
+        team1Name: team1Name ?? this.team1Name,
+        team2Name: team2Name ?? this.team2Name,
+        team1Score: team1Score ?? this.team1Score,
+        team2Score: team2Score ?? this.team2Score,
+        result: result ?? this.result,
+        attendancdCount: attendancdCount ?? this.attendancdCount,
+        nonappearanceCount: nonappearanceCount ?? this.nonappearanceCount,
+      );
+
+  factory Matchinfo.fromRawJson(String str) => Matchinfo.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+    factory Matchinfo.fromJson(Map<String, dynamic> json) => Matchinfo(
     idx: json["idx"],
     team1Id: json["team1_id"],
     team2Id: json["team2_id"],
@@ -63,6 +105,8 @@ class Matchinfo {
     team1Score: json["team1_score"],
     team2Score: json["team2_score"],
     result: json["result"],
+    attendancdCount: json["attendancd_count"],
+    nonappearanceCount: json["nonappearance_count"],
   );
 
   Map<String, dynamic> toJson() => {
@@ -77,5 +121,7 @@ class Matchinfo {
     "team1_score": team1Score,
     "team2_score": team2Score,
     "result": result,
+    "attendancd_count": attendancdCount,
+    "nonappearance_count": nonappearanceCount,
   };
 }
